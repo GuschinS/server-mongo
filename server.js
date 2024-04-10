c494c957801aa36df834e1bca36c27c899ec9b1a
Sergei Gushchin <guschins@gmail.com>
2024-04-10 22:51:49 +0200

    change access control

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'all');
+    res.header('Access-Control-Allow-Origin', 'https://www.onoranzepet.it');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
